d8ba80e51ccfb602349edd67f34d2311cd469f6e
Muntasir Billah <149241721+Muntasir-UwU@users.noreply.github.com>
2025-06-22 08:52:47 +0000

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders pool party header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/pool party/i);
+  expect(heading).toBeInTheDocument();
 });
